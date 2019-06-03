94098c997e41e3d22c0e6365ff9c61ab7ae483d2
wiktoradamczyk <wiktoradamczyk284@gmail.com>
2019-06-03 20:21:53 +0200

    Initial commit

---
+++ Gra.Historia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelGry
+{
+    public partial class Gra
+    {
+        private readonly List<Ruch> historia;
+        public IReadOnlyList<Ruch> Historia => historia;
+
+        // inner class - klasa wewnętrzna
+        public class Ruch
+        {
+            public readonly int propozycja;
+            public readonly Odpowiedz odpowiedz;
+            public readonly DateTime kiedy;
+
+            public Ruch(int prop, Odpowiedz odp)
+            {
+                propozycja = prop;
+                odpowiedz = odp;
+                kiedy = DateTime.Now;
+            }
+
+            public override string ToString()
+            {
+                return $"({propozycja}, {odpowiedz}, {kiedy})";
+            }
+        }
+
+    }
+}
---
+++ Gra.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelGry
+{
+    public partial class Gra
+    {
+        // inner types
+        public enum Odpowiedz { ZaMalo = -1, Trafiono = 0, ZaDuzo = 1 }
+        public enum State { Trwa, Poddana, Odgadnieta }
+
+        //fields
+        public State StanGry { get; private set; }
+        public readonly int ZakresOd;
+        public readonly int ZakresDo;
+        private readonly int wylosowana;
+        public int? Wylosowana
+        {
+            get
+            {
+                if (StanGry != State.Trwa)
+                    return wylosowana;
+                else
+                    return null;
+            }
+            //set { }
+        }
+
+        // historia gry: ToDo
+        public int LicznikRuchow { get; private set; } = 0;
+
+        public Gra(int a, int b)
+        {
+            ZakresOd = Math.Min(a,b);
+            ZakresDo = Math.Max(a,b);
+            wylosowana = Losuj(ZakresOd, ZakresDo);
+            //LicznikRuchow = 0;
+            StanGry = State.Trwa;
+            historia = new List<Ruch>();
+        }
+
+        public Odpowiedz Ocena( int propozycja )
+        {
+            LicznikRuchow++;
+            Odpowiedz odp;
+            if (propozycja < wylosowana)
+                odp = Odpowiedz.ZaMalo;
+            else if (propozycja > wylosowana)
+                odp = Odpowiedz.ZaDuzo;
+            else
+            {
+                StanGry = State.Odgadnieta;
+                odp = Odpowiedz.Trafiono;
+            }
+            historia.Add(new Ruch(propozycja, odp));
+            return odp;
+        }
+
+        public void Poddaj()
+        {
+            StanGry = State.Poddana;
+        }
+
+        public static int Losuj(int min = 1, int max = 100)
+        {
+            if (min > max)
+            { //swap
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            Random generator = new Random();
+            return generator.Next(min, max + 1);
+        }
+    }
+}
